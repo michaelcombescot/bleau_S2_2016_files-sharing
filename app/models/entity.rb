ec22307f0419b4aa334cbde3ac604cedd5584bf0
michaelcombescot <michael.combescot@gmail.com>
2017-02-02 17:15:02 +0100

    ajout verif non duspli de nom au sign up

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -6,6 +6,8 @@ class Entity < ApplicationRecord
 	has_many :shared_withs
 	has_many :media, through: :shared_withs
 
+	validates_uniqueness_of :name
+
 	accepts_nested_attributes_for :shared_withs
 
 	scope :all_groups, -> { where('type_type  = ?', 'Group') }
